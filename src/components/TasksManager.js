5d124d8a10d62ed26225a94635a3915528500010
Karolina <karolina.jedrajczyk@gmail.com>
2025-05-22 10:43:09 +0200

    adding tasks to data

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React from "react";
 
 class TasksManager extends React.Component {
-    state = {
-        tasks: [],
-    }
+  state = {
+    task: "",
+    tasks: [],
+  };
 
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
-    }
+  handleChange = (e) => {
+    console.log("handleChange", e.target.value);
+    this.setState({ task: e.target.value });
+  };
 
-    render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
-    }
+  componentDidMount() {
+    fetch("http://localhost:3005/data")
+      .then((res) => res.json())
+      .then((data) => this.setState({ tasks: data }));
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handleSubmit", this.state.task);
+
+    const newTask = {
+      name: this.state.task,
+      time: 0,
+      isRunning: false,
+      isDone: false,
+      isRemoved: false,
+    };
+
+    fetch("http://localhost:3005/data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTask),
+    })
+      .then((res) => res.json())
+      .then((taskFromServer) => {
+        this.setState((state) => ({
+          tasks: [...state.tasks, taskFromServer],
+          task: "",
+        }));
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="task"
+            value={this.state.task}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Dodaj zadanie</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default TasksManager;
